7938b1a8750c50e9827fdc432487b358e5fa4793
Aakash Sangwan <47134609+Aakash2408@users.noreply.github.com>
2021-10-23 14:49:13 +0530

    Update urls.js

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,4 @@
 module.exports = {
     local : "http://localhost:8080",
-    // production: process.env.REACT_APP_CUSTOM_ENV_VAR
+    production: process.env.REACT_APP_CUSTOM_ENV_VAR
 }
-// db to aa rh ah is baar frotnend restart krta hu
